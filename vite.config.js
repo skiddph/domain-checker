7c75a09eba0cf35e0a96e9f4a00d3ee25869d3ca
eru123 <yeoligoakino@gmail.com>
2022-03-05 23:51:06 +0800

    added vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [ vue() ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  base: '',
+  build: {
+    extractCSS: true,
+    minify: 'terser',
+    outDir: 'docs'
+  }
 })
