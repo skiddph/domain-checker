39307587e5c74b44c76e7e6d7596b098f8f62d05
eru123 <yeoligoakino@gmail.com>
2022-03-05 23:50:02 +0800

    added entry js module

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import '@/styles/global.scss'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCamera, faCameraRotate, faLightbulb, faSpinner, faFileImport, faFileExport,faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCamera)
+library.add(faCameraRotate)
+library.add(faLightbulb)
+library.add(faSpinner)
+library.add(faFileImport)
+library.add(faFileExport)
+library.add(faTrash)
+
+const app = createApp(App)
+app.component('icon', FontAwesomeIcon)
+app.mount('#app')
