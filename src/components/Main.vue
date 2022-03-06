d0a8279b4165b3be99cc94343654db49b97a1a17
eru123 <yeoligoakino@gmail.com>
2022-03-06 22:26:16 +0800

    optimize build

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="__domain-checker__component__main-wrapper">
-    <div class="container">
+  <div class="w">
+    <div class="c">
       <slot />
     </div>
   </div>
@@ -8,10 +8,10 @@
 <style lang="scss" scoped>
 @import '@/styles/variables.scss';
 
-.__domain-checker__component__main-wrapper {
+.w {
   @apply w-full flex flex-row items-center justify-center;
 
-  .container {
+  .c {
     @apply w-full py-8 px-4;
     max-width: $max-width;
     min-height: 50vh;
