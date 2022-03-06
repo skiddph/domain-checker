2ee19d0a572d0f3cfc964588660ff6ca5d97c21b
eru123 <yeoligoakino@gmail.com>
2022-03-06 22:26:09 +0800

    optimize build

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,15 @@
 <script setup>
 
 let year = new Date(Date.now()).getFullYear();
-if(year < 2022) year = 2022;
+if (year < 2022) year = 2022;
 
 </script>
 <template>
-  <div class="__domain-checker__component__footer-wrapper">
-    <div class="container">
+  <div class="w">
+    <div class="c">
       <slot>
         <div>
-          <h1>C {{year}}</h1>
+          <h1>C {{ year }}</h1>
           <p>This is a Footer.</p>
         </div>
       </slot>
@@ -17,10 +17,10 @@ if(year < 2022) year = 2022;
   </div>
 </template>
 <style lang="scss" scoped>
-@import '@/styles/variables.scss';
-.__domain-checker__component__footer-wrapper {
+@import "@/styles/variables.scss";
+.w {
   @apply w-full flex flex-row items-center justify-center;
-  .container {
+  .c {
     @apply w-full flex flex-col items-center justify-between py-12 px-4 border-t text-sm text-center text-gray-400;
     max-width: $max-width;
   }
