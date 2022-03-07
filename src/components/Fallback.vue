eb383480ba521ca19368339b44eee740e1943f9f
eru123 <yeoligoakino@gmail.com>
2022-03-07 16:03:19 +0800

    added fallback component

---
+++ src/components/Fallback.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="c">
+    <h3>Type your query.</h3>
+    <h3>Select Vendor (ex: freenom).</h3>
+    <h3>Press Search button.</h3>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .c {
+    @apply w-full h-full flex flex-col items-center justify-center my-24 text-gray-400;
+  }
+</style>
