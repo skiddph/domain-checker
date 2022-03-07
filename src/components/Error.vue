73713945e0d31b8589c3e08d5bf83c7743d3f64a
eru123 <yeoligoakino@gmail.com>
2022-03-07 16:02:07 +0800

    added error component

---
+++ src/components/Error.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  message: String,
+})
+
+</script>
+<template>
+  <div class="c" >
+    <icon icon="triangle-exclamation" />
+    <p class="message">{{ message }}</p>
+  </div>
+</template>
+<style lang="scss" scoped>
+.c {
+  @apply flex flex-col items-center justify-center;
+
+  svg {
+    @apply text-4xl text-red-500 mt-12 mb-6;
+  }
+  .message {
+    @apply text-base text-gray-700;
+    
+  }
+}
+</style>
