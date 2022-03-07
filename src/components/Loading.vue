05f9d5fabd8ef6b03ca65373f70c9f902dfd5efe
eru123 <yeoligoakino@gmail.com>
2022-03-07 16:02:41 +0800

    added loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="c">
+    <icon icon="spinner" />
+  </div>
+</template>
+<style lang="scss" scoped>
+.c {
+  @apply flex flex-row items-center justify-center my-16;
+  
+  svg {
+    @apply text-6xl text-gray-500;
+    animation: spin 1.2s linear infinite;
+  }
+
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+}
+</style>
