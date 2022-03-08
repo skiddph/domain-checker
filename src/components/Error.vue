95798de3d049a90f9930b2361a9e15c8c0dd5b3a
eru123 <yeoligoakino@gmail.com>
2022-03-09 05:58:27 +0800

    update style, fix typo

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -5,11 +5,12 @@ const props = defineProps({
 
 </script>
 <template>
-  <div class="c" >
+  <div class="c">
     <icon icon="triangle-exclamation" />
     <p class="message">{{ message }}</p>
     <p class="cors">
-      This might be cause by CORS policy issue. Click this <a target="_blank" href="https://cors-anywhere.herokuapp.com/corsdemo">link</a> and request a temporary access and go back here.
+      This might be cause by CORS policy issue. Click this
+      <a target="_blank" href="https://cors-anywhere.herokuapp.com/corsdemo">link</a>, request a temporary access and retry your search.
     </p>
   </div>
 </template>
@@ -21,11 +22,11 @@ const props = defineProps({
     @apply text-4xl text-red-500 mt-12 mb-6;
   }
   .message {
-    @apply text-base text-gray-700;
+    @apply text-base text-red-400;
   }
 
   .cors {
-    @apply text-sm my-12 mx-24 text-center text-gray-500;
+    @apply text-sm my-12  w-3/4 md:w-1/2 text-center text-gray-500;
 
     a {
       @apply text-blue-500 hover:underline;
