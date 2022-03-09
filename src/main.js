e2f6d347f63be198ba4840de4d70dcddf9ddc943
eru123 <yeoligoakino@gmail.com>
2022-03-09 10:58:28 +0800

    added filter icon

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import App from './App.vue'
 import '@/styles/global.scss'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMagnifyingGlass, faX, faCircle, faTriangleExclamation, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faX, faCircle, faTriangleExclamation, faSpinner, faFilter } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faMagnifyingGlass)
 library.add(faX)
 library.add(faCircle)
 library.add(faTriangleExclamation)
 library.add(faSpinner)
+library.add(faFilter)
 
 const app = createApp(App)
 app.component('icon', FontAwesomeIcon)
