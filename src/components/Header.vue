1ef9ca86d76e409db4c7bd89ec41f4734126b186
eru123 <yeoligoakino@gmail.com>
2022-03-09 12:51:44 +0800

    added darkMode toggle btn

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,20 @@
+<script setup>
+import { ref, watch } from 'vue'
+const dm = ref(localStorage.getItem('darkMode') == 'true')
+
+watch(dm, (e) => e ? darkMode() : lightMode())
+</script>
 <template>
   <div class="w">
     <div class="c">
       <div class="brand">
         <div class="l">
-          <img src="/logo.png" alt="logo" />
-          <h1>Domain Checker</h1>
+          <img src="/logo.png" alt="logo" title="Skidd PH logo"/>
+          <h1 title="App title">Domain Checker</h1>
         </div>
+      </div>
+      <div class="toggle">
+        <icon class="dmt" :icon="dm ? 'toggle-on' : 'toggle-off'" @click="dm = !dm" title="Toggle Dark mode"/>
       </div>
     </div>
   </div>
@@ -16,7 +25,7 @@
   @apply w-full sticky top-0 z-50 bg-white border-b flex flex-row items-center justify-center;
 
   .c {
-    @apply w-full flex flex-row items-center justify-between py-3 px-4;
+    @apply w-full flex flex-row items-center justify-between py-2 px-4;
     max-width: $max-width;
 
     .l {
@@ -31,6 +40,14 @@
         font-family: "Fredoka", sans-serif;
       }
     }
+
+    .toggle {
+      .dmt {
+        @apply text-xl cursor-pointer p-2 bg-inherit rounded-full;
+        @apply hover:bg-gray-900 hover:text-gray-100;
+        @apply transition-all duration-200;
+      }
+    }
   }
 }
 .dark {
@@ -40,6 +57,12 @@
       .l {
         h1 {
           @apply text-blue-200;
+        }
+      }
+      .toggle {
+        .dmt {
+          @apply text-blue-400;
+        @apply hover:bg-blue-400 hover:text-gray-900;
         }
       }
     }
