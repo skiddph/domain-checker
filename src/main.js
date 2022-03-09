cdc3fb602c18a7d6414ccb963331ecb138d702a0
eru123 <yeoligoakino@gmail.com>
2022-03-09 11:25:13 +0800

    added darkMode switcher

--- src/main.js
+++ src/main.js
@@ -13,6 +13,28 @@ library.add(faTriangleExclamation)
 library.add(faSpinner)
 library.add(faFilter)
 
+window.darkMode = function () {
+  document.body.querySelector('#app').className = 'dark'
+  localStorage.setItem('darkMode', true)
+}
+
+window.lightMode = function () {
+  document.body.querySelector('#app').className = ''
+  localStorage.setItem('darkMode', false)
+}
+
+window.getAppMode = function () {
+  const dark = (
+    localStorage.getItem('darkMode') !== null ?
+    localStorage.getItem('darkMode') == 'true' : 
+    null
+  ) || false;
+  console.log('dark', dark)
+  if (dark == true) darkMode();
+  else lightMode();
+}
+
+getAppMode()
 const app = createApp(App)
 app.component('icon', FontAwesomeIcon)
 app.mount('#app')
